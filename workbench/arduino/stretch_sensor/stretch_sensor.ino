b504c37e35d5e3361c89302479ec0843035c87d7
Martin F. Stoelen <mfstolen@gmail.com>
2015-10-01 21:51:05 +0100

    Less prints.

--- workbench/arduino/stretch_sensor/stretch_sensor.ino
+++ workbench/arduino/stretch_sensor/stretch_sensor.ino
@@ -2,11 +2,12 @@
 #include <Servo.h>
 
 int sensor_pin = A0;
-int size_running = 15;
+int size_running = 50;
 
 int sensor_value = 0; 
 int servo_value = 0;
 float average = 0.0;
+int counter = 0;
 
 Servo my_servo;
 RunningAverage running(size_running);
@@ -19,23 +20,28 @@ void setup() {
 
 void loop() {
   sensor_value = analogRead(sensor_pin); 
-  Serial.print("Measured voltage: ");
-  Serial.print(sensor_value);
   
   servo_value = map(sensor_value, 970, 990, 0, 179);
   running.addValue(servo_value);
   average = running.getAverage();
   
-  if ((average >= 0) || (average <= 255)) {
+  if ((average >= 0) && (average <= 255)) {
     my_servo.write(average);
-    Serial.print(", servo output (averaged): ");
-    Serial.print(average);
-    Serial.print(".");
+    
+    if(counter > 20) {
+      Serial.print("Input: ");
+      Serial.print(sensor_value);
+      Serial.print(", servo output (averaged): ");
+      Serial.print(average);
+      Serial.print(".");
+      Serial.println();
+      counter = 0;
+    }
   }
   else {
-    Serial.print(" and outside range.");
-  }
-  Serial.println(); 
+    Serial.println("Outside range.");
+  } 
   
+  counter++;
   delay(5);  
 }
