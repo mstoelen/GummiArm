f61165c2f51d73a61b27cc7f40cd4e6b3fd74572
Martin F. Stoelen at COSMOS <mfstolen@gmail.com>
2016-03-03 19:00:01 +0000

    More full arm tests.
    Former-commit-id: a7afa3d6969d38b1514aeddda2bd8607868cee77

--- orchestration/packages/src/gummi_interface/scripts/gummi_step_full_arm.py
+++ orchestration/packages/src/gummi_interface/scripts/gummi_step_full_arm.py
@@ -9,16 +9,16 @@ from gummi_interface.gummi import Gummi
 def main(args):
 
     pi = 3.1416
-    print("Please enter path to folder where you want data file saved:")
-    path =  raw_input()
 
     rospy.init_node('gummi', anonymous=True)
     r = rospy.Rate(60)  
     
     cocontractionsToTry = (0.0, 0.25, 0.5, 0.75, 1.0)
 
-    rest = (-0.13, -0.3, 0.89, -0.26, -0.05, 0.69, 0.6)
-    desired = (0.16, 0.56, 0.35, -0.56, 0.04, 0.40, -0.26)
+    #rest = (-0.13, -0.3, 0.89, -0.26, -0.05, 0.69, 0.6)
+    rest = (-0.29, -0.40, 0.68, -0.14, 0.14, 0.46, 0.39)
+    #desired = (0.16, 0.56, 0.35, -0.56, 0.04, 0.40, -0.26)
+    desired = (0.44, 0.99, 0.06, -1.00, -0.56, 2.29, -0.43)
 
     gummi = Gummi()
 
@@ -48,42 +48,27 @@ def main(args):
                 r.sleep()
             print("Test started, cocontraction: " + str(cocont) + ", attempt: " + str(att) + ".")
             
-            fileName = path + '/step_test_full_arm_s_' + str(cocont) + '_a_' + str(att) + '.csv'
-            with open(fileName, 'wb') as csvfile:
-                #writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                #writer.writerow(['time','desired','angle', 'equilibrium', 'flexor', 'extensor'])
+            time1 = rospy.Time.now()
+            now = False
+            for i in range (0,1000):
                 
-                time1 = rospy.Time.now()
-                now = False
-                for i in range (0,1000):
-
-                    if i < 200:
-                        command = rest
+                if i < 200:
+                    command = rest
+                else:
+                    if i < 600:
+                        command = desired
+                        now = False
+                        if i == 200:
+                            now = True
                     else:
-                        if i < 600:
-                            command = desired
-                            now = False
-                            if i == 200:
-                                now = True
-                        else:
-                            command = rest
-                            now = False
-                            if i == 600:
-                                now = True
+                        command = rest
+                        now = False
+                        if i == 600:
+                            now = True
+                            
+                gummi.goTo(command, now)
 
-                    gummi.goTo(command, now)
-
-                    #angle = joint.getJointAngle() * 180/pi
-                    #time2 = rospy.Time.now()
-                    #duration = time2-time1
-                    #delta = duration.to_sec()
-                    #equilibrium = joint.getDesiredEquilibrium()
-                    #cocontraction = joint.getCommandedCocontraction()
-                    #flexor = joint.getFlexorAngle()
-                    #extensor = joint.getExtensorAngle()
-
-                    #writer.writerow([delta, command, angle, equilibrium, cocontraction, flexor, extensor])
-                    r.sleep()
+                r.sleep()
             
 if __name__ == '__main__':
     main(sys.argv)
