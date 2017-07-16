00b3473e561a13317fcd0e2982c1fffa3096591e
jwalker930 <jonathan.walker@postgrad.plymouth.ac.uk>
2017-07-16 14:29:34 +0100

    moveit working using cartesian path

--- orchestration/packages/src/gummi_door/scripts/reach.py
+++ orchestration/packages/src/gummi_door/scripts/reach.py
@@ -2,6 +2,7 @@
 
 import sys
 import rospy
+import copy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
@@ -27,7 +28,8 @@ class planning():
         return self.x, self.y, self.z
 
     def main(self):
-
+        rospy.sleep(5
+        )
         moveit_commander.roscpp_initialize(sys.argv)
         robot = moveit_commander.RobotCommander()
         scene = moveit_commander.PlanningSceneInterface()
@@ -37,7 +39,7 @@ class planning():
         #y = 0.1
         #z = 0.1
 
-
+        #group.setPlanningTime(10)
         display_trajectory_publisher = rospy.Publisher(
                                             '/move_group/display_planned_path',
                                             moveit_msgs.msg.DisplayTrajectory,
@@ -51,22 +53,52 @@ class planning():
         print "============ Printing robot state"
         print robot.get_current_state()
         print ("============ Generating plan 1")
-        print "Pose=>",group.get_current_pose().pose
-        pose_target = geometry_msgs.msg.Pose()
-        pose_target.orientation.w = 1.0
-        pose_target.position.x = self.x
-        pose_target.position.y = -self.y
-        pose_target.position.z = self.z
-        group.set_pose_target(pose_target)
+        waypoints = []
 
-        plan1 = group.plan()
+        # start with the current pose
+        waypoints.append(group.get_current_pose().pose)
+
+        # first orient gripper and move forward (+x)
+        wpose = geometry_msgs.msg.Pose()
+        wpose.orientation.w = -0.5
+        wpose.position.x = self.x
+        wpose.position.y = self.y
+        wpose.position.z = self.z
+        waypoints.append(copy.deepcopy(wpose))
+        rospy.sleep(1)
+        # second move down
+        wpose.position.z -= 0.10
+        waypoints.append(copy.deepcopy(wpose))
+        rospy.sleep(1)
+        # third move to the side
+        #wpose.position.y += 0.05
+        #waypoints.append(copy.deepcopy(wpose))
+
+        (plan3, fraction) = group.compute_cartesian_path(
+                                     waypoints,   # waypoints to follow
+                                     0.01,        # eef_step
+                                     0.0)         # jump_threshold
+
+        print "============ Waiting while RVIZ displays plan3..."
+        rospy.sleep(5)
+
+        #print "Pose=>",group.get_current_pose().pose
+        #pose_target = geometry_msgs.msg.Pose()
+        #pose_target.orientation.w = 1.0
+        #pose_target.position.x = self.x
+        #pose_target.position.y = -self.y
+        #pose_target.position.z = self.z
+        #print (pose_target)
+        #group.set_pose_target(pose_target)
+
+        plan3 = group.plan()
         print ("============ Waiting while RVIZ displays plan1...")
         rospy.sleep(1)
         print ("============ Visualizing plan1")
         display_trajectory = moveit_msgs.msg.DisplayTrajectory()
 
         display_trajectory.trajectory_start = robot.get_current_state()
-        display_trajectory.trajectory.append(plan1)
+        display_trajectory.trajectory.append(plan3)
         display_trajectory_publisher.publish(display_trajectory)
         print ("============ Waiting while plan1 is visualized (again)...")
         rospy.sleep(1)
